2e9f1e875c4acab14ed18ef5da85a59fc7f8c62e
Rafael Cerri <rafaelbcerri@gmail.com>
2018-08-05 16:31:38 -0300

    Finish

---
+++ public/firebase-init.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCT7xDyOTmfxMZxGLkFWsqrNApMUNrm2UM",
+  authDomain: "class-prep.firebaseapp.com",
+  databaseURL: "https://class-prep.firebaseio.com",
+  projectId: "class-prep",
+  storageBucket: "class-prep.appspot.com",
+  messagingSenderId: "829209543624"
+};
+
+firebase.initializeApp(config);
